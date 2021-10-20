27e0c2afde07d88402256dd0831258a20b263add
dlosemy <dlosemy@gmail.com>
2021-10-20 23:44:21 +0900

    neo4j 와 연결 부분 추가

--- crawl.py
+++ crawl.py
@@ -85,5 +85,54 @@ for idx_line in range(len(df)):
 df = df[df['media'] != '코리아헤럴드']    # 코리아헤럴드는 영어 제목임
 df = df[df['media'] != '주간경향']    # 주간경향은 같은 title이 많음
 
-print("df: ", df['keyword'])
-df.to_csv('test.csv',index=False)
+
+from neo4j import GraphDatabase
+
+
+""" make node & relationship"""
+def add_news(tx, title, date, media, keyword):
+    tx.run("MERGE (a:News {title: $title , date: $date, media: $media, keyword: $keyword})",
+           title=title, date=date, media=media, keyword=keyword)
+
+
+def add_media(tx):
+    tx.run("MATCH (a:News) "
+           "MERGE (b:Media {name:a.media}) "
+           "MERGE (a)<-[r:Publish]-(b)")
+
+
+def add_keyword(tx):
+    tx.run("MATCH (a:News) "
+           "UNWIND a.keyword as k "
+           "MERGE (b:Keyword {name:k}) "
+           "MERGE (a)-[r:Include]->(b)")
+
+
+
+""" 한자와 공백 제거 """
+# Neo4j -> Gephi 에서 parsing error의 원인이 될 수 있음
+def clean_text_for_neo4j(row):
+    text = row['title_c']
+    text = re.sub(pattern='[^a-zA-Z0-9ㄱ-ㅣ가-힣]', repl='', string=text)
+    # print("영어, 숫자, 한글만 포함 : ", text )
+    return text
+
+df['title_c_neo4j'] = df.apply(clean_text_for_neo4j, axis=1)
+
+
+
+""" 연결 """
+# Neo4j 브라우저에서 설정한 계정의 ID, PASSWORD를 통해 접속
+greeter = GraphDatabase.driver("bolt://localhost:7687", auth=("test1019", "test1019"))  
+
+
+
+""" 입력 """
+# Cyper code를 이용,  크롤링한 Data를 DB에 입력
+with greeter.session() as session:
+    """ make node """
+    for idx in range(len(df)):
+        session.write_transaction(add_article, title=df.iloc[idx]['title_c_neo4j'], date=df.iloc[idx]['date'],
+                                  media=df.iloc[idx]['media'], keyword=list(df.iloc[idx]['keyword']))
+    session.write_transaction(add_media)
+    session.write_transaction(add_keyword)
